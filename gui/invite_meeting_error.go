90b00ae61019a9f8c38ab4e2b318614b7ba7389e
Pedro Palau <pepalauisaac@gmail.com>
2020-01-15 23:30:19 -0500

    Removes unnecessary comment

--- gui/invite_meeting_error.go
+++ gui/invite_meeting_error.go
@@ -6,14 +6,4 @@ func (u *gtkUI) openErrorDialog(message string) {
 		finalMessage = "The meeting ID is invalid"
 	}
 	u.reportError(finalMessage)
-	// builder := u.g.uiBuilderFor("InviteCodeErrorWindow")
-	// win := builder.get("inviteCodeErrorWindow").(gtki.ApplicationWindow)
-	// win.SetApplication(u.app)
-
-	// builder.ConnectSignals(map[string]interface{}{
-	// 	"on_ok": func() {
-	// 		win.Hide()
-	// 	},
-	// })
-	// win.ShowAll()
 }
