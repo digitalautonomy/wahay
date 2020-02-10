9347470a50de70f1994152a64c8a63fece0d7aea
rafael <rafael@autonomia.digital>
2020-02-10 14:23:18 -0500

    Creates, hashes, sign bundles and send them to the server. Not yet publish in the website

--- deployment/create-bundles.sh
+++ deployment/create-bundles.sh
@@ -2,23 +2,38 @@
 
 set -x
 
+DISTRO_FILE="supported-bundle-distros.txt"
+APP_NAME=wahay
+BINARY_BASE_NAME=$(basename $BINARY_NAME)
+BINARY_VERSION=${BINARY_BASE_NAME#$APP_NAME}
 
 # Create working directories
-mkdir -p generate-bundles/wahay
+mkdir generate-bundles
 mkdir publish-bundles
 
 # Download Binary  Bundles from Nextcloud
 cd generate-bundles
 
 echo "$RCLONE_CONFIG" > rclone.conf
-rclone copy --config rclone.conf  wahay: .
+rclone copy --config rclone.conf  $APP_NAME: .
 
 #extract tor
-tar xvf tor-0.4.2.5.tar.bz2 --directory wahay
+tar xvf tor-0.4.2.5.tar.bz2
 
-# Create Ubuntu 18.04 Bundle
-tar xvf mumble-ubuntu-18-04.tar.bz2 --directory wahay
-cp ../$BINARY_NAME wahay/wahay
-tar cjf wahay-bundle-ubuntu-18-04.tar.bz2 wahay
-rm -rf wahay/mumble
-mv wahay-bundle-ubuntu-18-04.tar.bz2 ../publish-bundles
+# Create distro bundles
+while IFS= read -r DISTRO_NAME
+do
+ DISTRO_DIR=${APP_NAME}-${DISTRO_NAME}${BINARY_VERSION}
+ mkdir $DISTRO_DIR
+ tar xvf mumble-${DISTRO_NAME}.tar.bz2 --directory $DISTRO_DIR
+ cp -r tor $DISTRO_DIR
+ cp ../$BINARY_NAME $DISTRO_DIR
+ cd $DISTRO_DIR
+ ln -s $BINARY_BASE_NAME $APP_NAME
+ cd ..
+ tar cjf wahay-${DISTRO_NAME}$BINARY_VERSION.tar.bz2 $DISTRO_DIR
+ sha256sum wahay-${DISTRO_NAME}$BINARY_VERSION.tar.bz2 > wahay-${DISTRO_NAME}$BINARY_VERSION.tar.bz2.sha256sum
+ gpg --detach-sign --armor -u 01242FFAB8CE1EC0C8F54456A8854162D28F171E wahay-${DISTRO_NAME}$BINARY_VERSION.tar.bz2.sha256sum
+ mv wahay-${DISTRO_NAME}$BINARY_VERSION.tar.bz2* ../publish-bundles
+done < "../$DISTRO_FILE"
+
