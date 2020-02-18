dcdd547a8c077701e7349f1e3482448c3d05f174
rafael <rafael@autonomia.digital>
2020-02-18 11:59:29 -0500

    Make hashes and sign hashes for deb packages #2

--- deployment/create-linux-packages.sh
+++ deployment/create-linux-packages.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+APP_NAME=wahay
+BINARY_BASE_NAME=$(basename $BINARY_NAME)
+BINARY_VERSION=${BINARY_BASE_NAME#$APP_NAME-}
+
 mkdir -p publish-linux-packages
 
 cd packaging
 make ubuntu-package-ci
 
-#print Ruby version for debugging
-ruby --version
-/usr/bin/ruby --version
+cd ../publish-linux-packages
+sha256sum ${APP_NAME}-ubuntu-${BINARY_VERSION}-amd64.deb > ${APP_NAME}-ubuntu-${BINARY_VERSION}-amd64.deb.sha256sum
+gpg --detach-sign --armor -u 01242FFAB8CE1EC0C8F54456A8854162D28F171E ${APP_NAME}-ubuntu-${BINARY_VERSION}-amd64.deb.sha256sum
