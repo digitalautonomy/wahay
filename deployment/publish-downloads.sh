ae284d86c1339b6d3918f8021c8f1d6cd3c3f979
rafael <rafael@autonomia.digital>
2020-02-18 10:16:17 -0500

    Copy deb packages to downloads directory

--- deployment/publish-downloads.sh
+++ deployment/publish-downloads.sh
@@ -9,6 +9,7 @@ TMP_DIR=~/tmp/deploy_binaries
 SUM_FILE_FULL=$(find $TMP_DIR -name "*.sha256sum" | grep -v ".bz2" | head -1)
 SHA256_SUM_FILE=$(basename $SUM_FILE_FULL)
 BINARY_SHA256_SUM=$(grep --only-matching -E "[[:xdigit:]]{64}" $SUM_FILE_FULL)
+BINARY_VERSION=${BINARY_NAME#$APP_NAME}
 BINARY_NAME=${SHA256_SUM_FILE%.sha256sum}
 SIGNATURE_FILE=$SHA256_SUM_FILE.asc
 ALL_BUNDLES=$(find $TMP_DIR -name  "*.bz2" -exec basename {} \;)
@@ -46,7 +47,7 @@ then
         fi
 
         ln -s $BINARY_NAME ${APP_NAME}-latest
-        ln -s $SHA256_SUM_FILE ${APP_NAME}-latest.sha256sum
+        ln -s $SHA256_SUM_FILE ${APP_NAME}-latest.sha256sum/
         ln -s $SIGNATURE_FILE ${APP_NAME}-latest.sha256sum.asc
 else
  
@@ -72,7 +73,7 @@ echo "$ALL_BUNDLES" | xargs -I file mv file $DOWNLOADS_DIR/bundles/$BINARY_NAME
 echo "$ALL_BUNDLES_SHA256_SUM" | xargs -I file mv file $DOWNLOADS_DIR/bundles/$BINARY_NAME
 echo "$ALL_BUNDLES_SIGNATURES" | xargs -I file mv file $DOWNLOADS_DIR/bundles/$BINARY_NAME
 
-
+# Create latest and tag name symlinks for linux bundles
 if [ $HAS_DATE -eq 0  ]
 then
         cd $DOWNLOADS_DIR
@@ -87,9 +88,36 @@ else
         WAHAY_TAG_NAME=${BINARY_NAME%-*******}
 
         cd $DOWNLOADS_DIR
-        rm  -rf $WAHAY_TAG_NAME*
+        rm  -rf $WAHAY_TAG_NAME*bz2*
 
 	echo "$ALL_BUNDLES" |  cut -d "-" -f 2,3 | xargs -I file find bundles/$BINARY_NAME -name "*file*.bz2" -exec ln -s {} $WAHAY_TAG_NAME-file.bz2 \;
 	echo "$ALL_BUNDLES" |  cut -d "-" -f 2,3 | xargs -I file find bundles/$BINARY_NAME -name "*file*.bz2.sha256sum" -exec ln -s {} $WAHAY_TAG_NAME-file.bz2.sha256sum \;
 	echo "$ALL_BUNDLES" |  cut -d "-" -f 2,3 | xargs -I file find bundles/$BINARY_NAME -name "*file*.bz2.sha256sum.asc" -exec ln -s {} $WAHAY_TAG_NAME-file.bz2.sha256sum.asc \;
+fi
+
+#Move Linux Packages to the Download directory
+cd $TMP_DIR
+mkdir -p $DOWNLOADS_DIR/linux-packages/$BINARY_NAME
+mv publish-linux-packages/wahay-ubuntu-$BINARY_VERSION-amd64.deb* $DOWNLOADS_DIR/linux-packages/$BINARY_NAME/
+
+#Create symlinks for Linux Packagres (just Ubuntu for now)
+if [ $HAS_DATE -eq 0  ]
+then
+        cd $DOWNLOADS_DIR
+        rm -f *latest.deb*
+
+	ln -s linux-packages/$BINARY_NAME/${APP_NAME}-ubuntu-$BINARY_VERSION-amd64.deb ${APP_NAME}-ubuntu-latest.deb
+	ln -s linux-packages/$BINARY_NAME/${APP_NAME}-ubuntu-$BINARY_VERSION-amd64.deb.sha256sum ${APP_NAME}-ubuntu-latest.deb.sha256sum
+	ln -s linux-packages/$BINARY_NAME/${APP_NAME}-ubuntu-$BINARY_VERSION-amd64.deb.sha256sum.asc ${APP_NAME}-ubuntu-latest.deb.sha256sum.asc
+else
+
+        #Retrieve WAHAY_TAG name
+        WAHAY_TAG_NAME=${BINARY_NAME%-*******}
+
+        cd $DOWNLOADS_DIR
+        rm  -rf $WAHAY_TAG_NAME*deb*
+
+	ln -s linux-packages/$BINARY_NAME/${APP_NAME}-ubuntu-$BINARY_VERSION-amd64.deb ${APP_NAME}-ubuntu-${WAHAY_TAG_NAME}.deb
+        ln -s linux-packages/$BINARY_NAME/${APP_NAME}-ubuntu-$BINARY_VERSION-amd64.deb.sha256sum ${APP_NAME}-ubuntu-${WAHAY_TAG_NAME}.deb.sha256sum
+        ln -s linux-packages/$BINARY_NAME/${APP_NAME}-ubuntu-$BINARY_VERSION-amd64.deb.sha256sum.asc ${APP_NAME}-ubuntu-${WAHAY_TAG_NAME}.deb.sha256sum.asc
 fi
