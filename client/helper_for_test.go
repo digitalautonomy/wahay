f24ce9e885dcd56b2f37855327bddcc36f814e7d
Fausto M. Lagos S <fausto@autonomia.digital>
2023-02-13 15:56:57 -0500

    Defined test package to do custom checkers available in all packages tests

--- client/helper_for_test.go
+++ client/helper_for_test.go
@@ -1,9 +1,7 @@
 package client
 
 import (
-	"fmt"
 	. "gopkg.in/check.v1"
-	"strings"
 	"testing"
 )
 
@@ -12,35 +10,3 @@ func Test(t *testing.T) { TestingT(t) }
 type clientSuite struct{}
 
 var _ = Suite(&clientSuite{})
-
-type stringMethodChecker struct {
-	*CheckerInfo
-	check func(string, string) bool
-}
-
-var Contains Checker = &stringMethodChecker{
-	&CheckerInfo{Name: "Contains", Params: []string{"value", "substring"}},
-	strings.Contains,
-}
-
-func (checker *stringMethodChecker) Check(params []interface{}, _ []string) (result bool, error string) {
-	return stringMethodCheck(params[0], params[1], checker.check)
-}
-
-func stringMethodCheck(value, expected interface{}, f func(string, string) bool) (result bool, error string) {
-	exStr, ok := expected.(string)
-	if !ok {
-		return false, "Expected must be a string"
-	}
-	valueStr, valueIsStr := value.(string)
-	if !valueIsStr {
-		if valueWithStr, valueHasStr := value.(fmt.Stringer); valueHasStr {
-			valueStr, valueIsStr = valueWithStr.String(), true
-		}
-	}
-	if valueIsStr {
-		checkOk := f(valueStr, exStr)
-		return checkOk, ""
-	}
-	return false, "Obtained value is not a string and has no .String()"
-}
