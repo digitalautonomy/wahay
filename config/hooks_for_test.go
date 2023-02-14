69b80d377467a0473fad62208f34548f98c9b5bd
Ola Bini <ola@olabini.se>
2023-02-14 18:15:41 +0000

    Add swallowing of logs to config tests

--- config/hooks_for_test.go
+++ config/hooks_for_test.go
@@ -1,9 +1,15 @@
 package config
 
 import (
+	"io"
 	"testing"
 
+	"github.com/sirupsen/logrus"
 	. "gopkg.in/check.v1"
 )
 
 func Test(t *testing.T) { TestingT(t) }
+
+func init() {
+	logrus.SetOutput(io.Discard)
+}
