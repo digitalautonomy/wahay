a145ea26b918c14cabe4577f3dff5ed9a0039ee0
Ola Bini <ola@olabini.se>
2023-05-05 02:30:19 -0500

    Fix up script a bit

--- deployment/create-bundles.sh
+++ deployment/create-bundles.sh
@@ -7,14 +7,11 @@
 
 set -xe
 
+export WAHAY_VERSION=0.1
 export TOR_VERSION=0.4.7.13
 
 DISTRO_FILE="../deployment/supported-bundle-distros.txt"
-APP_NAME=wahay
 BINARY_BASE_NAME=$(basename $BINARY_NAME)
-
-# Binary version will either be tag or 2023-04-23-ffaabbd
-BINARY_VERSION=${BINARY_BASE_NAME#$APP_NAME-}
 
 # Create working directories
 mkdir generate-bundles
@@ -26,17 +23,17 @@ tar xf ../tmp_bundle/linux-x86_64-tor-$TOR_VERSION.tar.gz
 # Create distro bundles
 while IFS= read -r DISTRO_NAME
 do
- DISTRO_DIR=${APP_NAME}-${DISTRO_NAME}-${BINARY_VERSION}
+ DISTRO_DIR=wahay-$DISTRO_NAME-$WAHAY_VERSION
  mkdir $DISTRO_DIR
- tar xf ../tmp_bundle/mumble-${DISTRO_NAME}-${APP_NAME}-${BINARY_VERSION}.tar.bz2 --directory $DISTRO_DIR
+ tar xf ../tmp_bundle/mumble-$DISTRO_NAME-wahay-$WAHAY_VERSION.tar.bz2 --directory $DISTRO_DIR
  cp -r tor $DISTRO_DIR
  cp -r ../packaging/bundles/* $DISTRO_DIR
  cp ../$BINARY_NAME $DISTRO_DIR
  cd $DISTRO_DIR
- ln -s $BINARY_BASE_NAME $APP_NAME
+ ln -s $BINARY_BASE_NAME wahay
  cd ..
- tar cjf wahay-${DISTRO_NAME}-$BINARY_VERSION.tar.bz2 $DISTRO_DIR
- sha256sum wahay-${DISTRO_NAME}-$BINARY_VERSION.tar.bz2 > wahay-${DISTRO_NAME}-$BINARY_VERSION.tar.bz2.sha256sum
- gpg --batch --detach-sign --armor -u 3EE89711B35F8B3089646FCBF3B1159FC97D5490 wahay-${DISTRO_NAME}-$BINARY_VERSION.tar.bz2.sha256sum
- mv wahay-${DISTRO_NAME}-$BINARY_VERSION.tar.bz2* ../publish-bundles
+ tar cjf wahay-$DISTRO_NAME-$WAHAY_VERSION.tar.bz2 $DISTRO_DIR
+ sha256sum wahay-$DISTRO_NAME-$WAHAY_VERSION.tar.bz2 > wahay-$DISTRO_NAME-$WAHAY_VERSION.tar.bz2.sha256sum
+ gpg --batch --detach-sign --armor -u 3EE89711B35F8B3089646FCBF3B1159FC97D5490 wahay-$DISTRO_NAME-$WAHAY_VERSION.tar.bz2.sha256sum
+ mv wahay-$DISTRO_NAME-$WAHAY_VERSION.tar.bz2* ../publish-bundles
 done < "$DISTRO_FILE"
