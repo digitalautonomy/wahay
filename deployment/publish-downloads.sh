aaaec0656bdf977cb5868648c8604ed8fa28137b
Ola Bini <ola@olabini.se>
2023-05-05 14:38:44 -0500

    Temporarily disable the publishing so I can manually debug it

--- deployment/publish-downloads.sh
+++ deployment/publish-downloads.sh
@@ -2,10 +2,12 @@
 
 set -xe
 
+export WAHAY_VERSION=0.1
+
 # Expect to have the new binary files, bundles per distributions and it's sha256sum and signature files
 
 APP_NAME=wahay
-TMP_DIR=~/tmp/deploy_binaries
+TMP_DIR=/home/wahay/tmp/deploy_binaries
 SUM_FILE_FULL=$(find $TMP_DIR -name "*.sha256sum" | grep -v ".bz2\|.deb\|.rpm" | head -1)
 SHA256_SUM_FILE=$(basename $SUM_FILE_FULL)
 BINARY_SHA256_SUM=$(grep --only-matching -E "[[:xdigit:]]{64}" $SUM_FILE_FULL)
@@ -23,9 +25,9 @@ DOWNLOADS_DIR=/usr/local/www/${APP_NAME}/downloads
 grep $BINARY_SHA256_SUM $DOWNLOADS_DIR/*.sha256sum 
 if [ $? -eq 0 ] 
 then
-        echo "Binary already exists"
-        rm -f $TMP_DIR/*
-        exit 0
+    echo "Binary already exists"
+    rm -f $TMP_DIR/*
+    exit 0
 fi
 
 #Move binaries to the download page
@@ -40,29 +42,29 @@ HAS_DATE=$?
 
 if [ $HAS_DATE -eq 0  ] 
 then
-        cd $DOWNLOADS_DIR
-        
-        if [ -L ${APP_NAME}-latest ] ; then
-                rm ${APP_NAME}-latest*
-        fi
-
-        ln -s $BINARY_NAME ${APP_NAME}-latest
-        ln -s $SHA256_SUM_FILE ${APP_NAME}-latest.sha256sum
-        ln -s $SIGNATURE_FILE ${APP_NAME}-latest.sha256sum.asc
-else
- 
-        #Retrieve WAHAY_TAG name        
-        WAHAY_TAG_NAME=$(echo ${BINARY_NAME%-*******})
-
-        cd $DOWNLOADS_DIR
-  
-        if [ -L $WAHAY_TAG_NAME ] ; then
-                rm $WAHAY_TAG_NAME*
-        fi
+    cd $DOWNLOADS_DIR
     
-        ln -s $BINARY_NAME $WAHAY_TAG_NAME
-        ln -s $SHA256_SUM_FILE $WAHAY_TAG_NAME.sha256sum
-        ln -s $SIGNATURE_FILE $WAHAY_TAG_NAME.sha256sum.asc
+    if [ -L ${APP_NAME}-latest ] ; then
+        rm ${APP_NAME}-latest*
+    fi
+
+    ln -s $BINARY_NAME ${APP_NAME}-latest
+    ln -s $SHA256_SUM_FILE ${APP_NAME}-latest.sha256sum
+    ln -s $SIGNATURE_FILE ${APP_NAME}-latest.sha256sum.asc
+else
+    
+    #Retrieve WAHAY_TAG name        
+    WAHAY_TAG_NAME=$(echo ${BINARY_NAME%-*******})
+
+    cd $DOWNLOADS_DIR
+    
+    if [ -L $WAHAY_TAG_NAME ] ; then
+        rm $WAHAY_TAG_NAME*
+    fi
+    
+    ln -s $BINARY_NAME $WAHAY_TAG_NAME
+    ln -s $SHA256_SUM_FILE $WAHAY_TAG_NAME.sha256sum
+    ln -s $SIGNATURE_FILE $WAHAY_TAG_NAME.sha256sum.asc
 
 fi
 
@@ -76,19 +78,18 @@ echo "$ALL_BUNDLES_SIGNATURES" | xargs -I file mv file $DOWNLOADS_DIR/bundles/$B
 # Create latest and tag name symlinks for linux bundles
 if [ $HAS_DATE -eq 0  ]
 then
-        cd $DOWNLOADS_DIR
-        rm -f *latest.tar.bz2*
+    cd $DOWNLOADS_DIR
+    rm -f *latest.tar.bz2*
 	
 	echo "$ALL_BUNDLES" |  cut -d "-" -f 1,2,3 | xargs -I file find bundles/$BINARY_NAME -name "file*.bz2" -exec ln -s {} file-latest.tar.bz2 \;
 	echo "$ALL_BUNDLES" |  cut -d "-" -f 1,2,3 | xargs -I file find bundles/$BINARY_NAME -name "file*.sha256sum" -exec ln -s {} file-latest.tar.bz2.sha256sum \;
 	echo "$ALL_BUNDLES" |  cut -d "-" -f 1,2,3 | xargs -I file find bundles/$BINARY_NAME -name "file*.asc" -exec ln -s {} file-latest.tar.bz2.sha256sum.asc \;
 else
+    #Retrieve WAHAY_TAG name
+    WAHAY_TAG_NAME=${BINARY_NAME%-*******}
 
-        #Retrieve WAHAY_TAG name
-        WAHAY_TAG_NAME=${BINARY_NAME%-*******}
-
-        cd $DOWNLOADS_DIR
-        rm  -rf $WAHAY_TAG_NAME*bz2*
+    cd $DOWNLOADS_DIR
+    rm  -rf $WAHAY_TAG_NAME*bz2*
 
 	echo "$ALL_BUNDLES" |  cut -d "-" -f 2,3 | xargs -I file find bundles/$BINARY_NAME -name "*file*.bz2" -exec ln -s {} $WAHAY_TAG_NAME-file.bz2 \;
 	echo "$ALL_BUNDLES" |  cut -d "-" -f 2,3 | xargs -I file find bundles/$BINARY_NAME -name "*file*.bz2.sha256sum" -exec ln -s {} $WAHAY_TAG_NAME-file.bz2.sha256sum \;
