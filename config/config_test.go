b4773976781e88979fe7d851e8b0e474fb5a0f66
Marcelo <marcelo@autonomia.digital>
2024-04-09 14:18:59 -0500

    Add mock for KeySupplier and use it in LoadFromFile() testing

--- config/config_test.go
+++ config/config_test.go
@@ -4,6 +4,7 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/stretchr/testify/mock"
 	. "gopkg.in/check.v1"
 )
 
@@ -16,7 +17,7 @@ func TestConfig(t *testing.T) { TestingT(t) }
 func (cs *ConfigSuite) Test_New_createsNewInstanceOfConfiguration(c *C) {
 	ac := New()
 
-	c.Assert(ac, Not(Equals), nil)
+	c.Assert(ac, NotNil)
 	c.Assert(ac.initialized, Equals, false)
 }
 
@@ -62,12 +63,43 @@ func (cs *ConfigSuite) Test_LoadFromFile_showErrorWhenPersistentConfigurationIsF
 
 }
 
-func (cs *ConfigSuite) Test_GenUniqueID_generatesUniqueID(c *C) {
+func (cs *ConfigSuite) Test_LoadFromFile_showErrorWhenPersistentConfigurationIsTrue(c *C) {
+    mockKeySupplier := MockKeySupplier{}
+
+    expectedKey := EncryptionResult{
+		key: []byte{0x01, 0x02, 0x03},
+		mac: []byte{0x04, 0x05, 0x06},
+		valid: true,
+	}
+    mockKeySupplier.On("GenerateKey", mock.Anything).Return(expectedKey)
+
+
+    ac := New()
+    ac.Init()
+
+
+    invalid, repeat, err := ac.LoadFromFile("example.json", &mockKeySupplier)
+
+
+    c.Assert(ac.initialized, Equals, true)
+    c.Assert(invalid, Equals, false)
+    c.Assert(repeat, Equals, false)
+    c.Assert(err, IsNil)
+}
+
+func (cs *ConfigSuite) Test_genUniqueID_generatesUniqueID(c *C) {
     ac := New()
     ac.genUniqueID()
 
     c.Assert(ac.UniqueConfigurationID, Not(Equals), "")
     c.Assert(len(ac.UniqueConfigurationID), Equals, 32*2)
+}
+
+func (cs *ConfigSuite) Test_GetUniqueID_returnsUniqueID(c *C) {
+	ac := New()
+	uniqueID := ac.GetUniqueID()
+
+	c.Assert(ac.UniqueConfigurationID, Equals, uniqueID)
 }
 
 func (cs *ConfigSuite) Test_WhenLoaded_addsFunctionToList(c *C) {
@@ -115,7 +147,66 @@ func (cs *ConfigSuite) Test_OnAfterSave_executesCallbacksCorrectly(c *C) {
 
     ac.onAfterSave()
 
-  
+
     c.Assert(flag, Equals, true)
     c.Assert(len(ac.afterSave), Equals, 0)
+}
+
+type MockKeySupplier struct {
+	mock.Mock
+}
+
+func (_m *MockKeySupplier) GenerateKey(p EncryptionParameters) EncryptionResult {
+	ret := _m.Called(p)
+
+	if len(ret) == 0 {
+		panic("no return value specified for GenerateKey")
+	}
+
+	var r0 EncryptionResult
+	if rf, ok := ret.Get(0).(func(EncryptionParameters) EncryptionResult); ok {
+		r0 = rf(p)
+	} else {
+		r0 = ret.Get(0).(EncryptionResult)
+	}
+
+	return r0
+}
+
+func (_m *MockKeySupplier) CacheFromResult(r EncryptionResult) error {
+	ret := _m.Called(r)
+
+	if len(ret) == 0 {
+		panic("no return value specified for CacheFromResult")
+	}
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(EncryptionResult) error); ok {
+		r0 = rf(r)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
+func (_m *MockKeySupplier) Invalidate() {
+	_m.Called()
+}
+
+func (_m *MockKeySupplier) LastAttemptFailed() {
+	_m.Called()
+}
+
+func NewMockKeySupplier(t interface {
+	mock.TestingT
+	Cleanup(func())
+
+}) *MockKeySupplier {
+	mock := &MockKeySupplier{}
+	mock.Mock.Test(t)
+
+	t.Cleanup(func() { mock.AssertExpectations(t) })
+
+	return mock
 }
