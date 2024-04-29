d436930d51205f7dd7b9a0d02602d34e7c70ffab
Laura Zambrano <blakami@autonomia.digital>
2024-04-29 13:30:47 -0500

    Test for newCertificate - validations when file do not exist.

--- hosting/certificate_test.go
+++ hosting/certificate_test.go
@@ -32,3 +32,32 @@ func (s *hostingSuite) Test_newCertificateServer_generatesAServerCertificateSucc
 	c.Assert(httpServer, NotNil)
 	c.Assert(err, IsNil)
 }
+
+func (s *hostingSuite) Test_newCertificateServer_returnsAnErrorWhenNoCertificateFileExistsOnPath(c *C) {
+	path := "/tmp/wahay"
+	var perm fs.FileMode = 0700
+	e := os.MkdirAll(path, perm)
+
+	if e != nil {
+		c.Fatalf("Failed to create temporary directory: %v", e)
+	}
+
+	defer os.RemoveAll(path)
+	httpServer, err := newCertificateServer(path)
+	expectedErr := "the certificate file do not exists"
+
+	c.Assert(err, NotNil)
+	c.Assert(err.Error(), Equals, expectedErr)
+	c.Assert(httpServer, IsNil)
+}
+
+func (s *hostingSuite) Test_newCertificate_returnsAnErrorWhenDirectoryDoNotExists(c *C) {
+	path := "fake/dir"
+	expectedErr := "the certificate file do not exists"
+
+	httpServer, err := newCertificateServer(path)
+
+	c.Assert(httpServer, IsNil)
+	c.Assert(err, NotNil)
+	c.Assert(err.Error(), Equals, expectedErr)
+}
