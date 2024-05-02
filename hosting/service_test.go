0c0732e9fb9e1f89cacd61d75adcb5d77269b3af
Laura Zambrano <blakami@autonomia.digital>
2024-05-02 11:38:11 -0500

    Tests for SetWelcomeText and first failing case of NewService.

--- hosting/service_test.go
+++ hosting/service_test.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"io/fs"
 
+	"github.com/digitalautonomy/wahay/tor"
 	"github.com/prashantv/gostub"
 	"github.com/stretchr/testify/mock"
 	. "gopkg.in/check.v1"
@@ -54,4 +55,23 @@ func (h *hostingSuite) Test_defaultHost_returnsLocalhostInterfaceWhenSomeKindOfE
 
 	c.Assert(dh, Equals, localhostInterface)
 	ms.AssertExpectations(c)
+}
+
+func (h *hostingSuite) Test_SetWelcomeText_worksWithBasicExample(c *C) {
+	srvc := &service{}
+	message := "This is a Wahay service test"
+	srvc.SetWelcomeText(message)
+	c.Assert(srvc.welcomeText, Equals, message)
+}
+
+func (h *hostingSuite) Test_NewService_returnsAnErrorWhenFailsCreatingCertificateServerBecauseNoDataDirectoryExists(c *C) {
+	servers := &servers{}
+	var ti tor.Instance
+	srvc, err := servers.NewService("1234", ti)
+
+	expectedErr := "the certificate file do not exists"
+
+	c.Assert(err, NotNil)
+	c.Assert(err.Error(), Equals, expectedErr)
+	c.Assert(srvc, IsNil)
 }
