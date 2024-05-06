b0e7f451c6815c68f8645d2ef8f97d59652e6044
steffano0 <steffanouromero@gmail.com>
2024-05-06 11:01:38 -0500

    Add test case for pathToConfig client method: returns and sets parent directory of binary if configuration directory is empty and path is not a directory.

--- client/config_test.go
+++ client/config_test.go
@@ -1,6 +1,9 @@
 package client
 
 import (
+	"os"
+	"path/filepath"
+
 	. "gopkg.in/check.v1"
 )
 
@@ -11,4 +14,30 @@ func (s *clientSuite) Test_pathToConfig_returnsClientConfigDirIfIsAlreadySetted(
 
 	c.Assert(pathToConfig, Equals, "path/to/config")
 
+}
+
+func (s *clientSuite) Test_pathToConfig_returnsAndSetsParentDirOfBinaryIfConfigDirIsEmptyAndPathIsBinary(c *C) {
+	tempDir, err := os.MkdirTemp("", "test")
+	if err != nil {
+		c.Fatalf("Failed to create temporary directory: %v", err)
+	}
+	defer os.RemoveAll(tempDir)
+
+	mumbleBinaryPath := filepath.Join(tempDir, "/mumble")
+	err = os.MkdirAll(mumbleBinaryPath, 0755)
+	if err != nil {
+		c.Fatalf("Failed to create directory: %v", err)
+	}
+
+	binaryFile, err := os.CreateTemp(mumbleBinaryPath, "mumble")
+	if err != nil {
+		c.Fatalf("Failed to create file")
+	}
+
+	client := &client{isValid: true, binary: &binary{path: binaryFile.Name()}, configDir: ""}
+
+	pathToConfig := client.pathToConfig()
+	expectedPath := filepath.Dir(binaryFile.Name())
+	c.Assert(pathToConfig, Equals, expectedPath)
+	c.Assert(client.configDir, Equals, expectedPath)
 }
