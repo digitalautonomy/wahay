efe0b9862cfae0ab8b556dd46cf1cb459634336b
Marcelo <marcelo@autonomia.digital>
2024-06-18 15:06:25 -0500

    [M] - Add write() test

--- client/db_test.go
+++ client/db_test.go
@@ -1,8 +1,41 @@
 package client
 
 import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
 	. "gopkg.in/check.v1"
 )
+
+func (s *clientSuite) Test_write_writesDBContent(c *C) {
+	tempDir, err := ioutil.TempDir("", "test")
+	c.Assert(err, IsNil)
+	defer os.RemoveAll(tempDir)
+
+	tempFile := filepath.Join(tempDir, "testfile.txt")
+
+	file, err := os.Create(tempFile)
+	c.Assert(err, IsNil)
+
+	defer file.Close()
+	defer os.Remove(tempFile)
+
+	content := []byte("example content")
+
+	db := &dbData{
+		filename: tempFile,
+		content:  content,
+	}
+
+	err = db.write()
+	c.Assert(err, IsNil)
+
+	readContent, err := ioutil.ReadFile(tempFile)
+
+	c.Assert(err, IsNil)
+	c.Assert(readContent, DeepEquals, content)
+}
 
 func (s *clientSuite) Test_exists_returnsTrueIfStringExistsInContent(c *C) {
 	db := &dbData{
