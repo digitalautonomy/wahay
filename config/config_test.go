9c3c45f62a5145cf8300a1b52fc1816286a8f1b4
Marcelo <marcelo@autonomia.digital>
2024-06-18 16:58:12 -0500

    Delete useless tests

--- config/config_test.go
+++ config/config_test.go
@@ -5,13 +5,12 @@ import (
 	"os"
 	"path/filepath"
 	"reflect"
-	"testing"
 
 	"github.com/stretchr/testify/mock"
 	. "gopkg.in/check.v1"
 )
 
-type ConfigSuite struct {}
+type ConfigSuite struct{}
 
 var _ = Suite(&ConfigSuite{})
 
@@ -28,7 +27,6 @@ func (cs *ConfigSuite) Test_Init_setsInitializedFieldValueToTrue(c *C) {
 
 	c.Assert(ac.initialized, Equals, true)
 }
-func TestConfig(t *testing.T) { TestingT(t) }
 
 func (cs *ConfigSuite) Test_InitDefault_initializesWithDefaultValues(c *C) {
 	ac := New()
@@ -76,35 +74,32 @@ func (cs *ConfigSuite) Test_LoadFromFile_showErrorWhenPersistentConfigurationIsF
 }
 
 func (cs *ConfigSuite) Test_LoadFromFile_showErrorWhenPersistentConfigurationIsTrue(c *C) {
-    mockKeySupplier := MockKeySupplier{}
+	mockKeySupplier := MockKeySupplier{}
 
-    expectedKey := EncryptionResult{
-		key: []byte{0x01, 0x02, 0x03},
-		mac: []byte{0x04, 0x05, 0x06},
+	expectedKey := EncryptionResult{
+		key:   []byte{0x01, 0x02, 0x03},
+		mac:   []byte{0x04, 0x05, 0x06},
 		valid: true,
 	}
-    mockKeySupplier.On("GenerateKey", mock.Anything).Return(expectedKey)
+	mockKeySupplier.On("GenerateKey", mock.Anything).Return(expectedKey)
 
+	ac := New()
+	ac.Init()
 
-    ac := New()
-    ac.Init()
+	invalid, repeat, err := ac.LoadFromFile("example.json", &mockKeySupplier)
 
-
-    invalid, repeat, err := ac.LoadFromFile("example.json", &mockKeySupplier)
-
-
-    c.Assert(ac.initialized, Equals, true)
-    c.Assert(invalid, Equals, false)
-    c.Assert(repeat, Equals, false)
-    c.Assert(err, IsNil)
+	c.Assert(ac.initialized, Equals, true)
+	c.Assert(invalid, Equals, false)
+	c.Assert(repeat, Equals, false)
+	c.Assert(err, IsNil)
 }
 
 func (cs *ConfigSuite) Test_genUniqueID_generatesUniqueID(c *C) {
-    ac := New()
-    ac.genUniqueID()
+	ac := New()
+	ac.genUniqueID()
 
-    c.Assert(ac.UniqueConfigurationID, Not(Equals), "")
-    c.Assert(len(ac.UniqueConfigurationID), Equals, 32*2)
+	c.Assert(ac.UniqueConfigurationID, Not(Equals), "")
+	c.Assert(len(ac.UniqueConfigurationID), Equals, 32*2)
 }
 
 func (cs *ConfigSuite) Test_GetUniqueID_returnsUniqueID(c *C) {
@@ -126,51 +121,49 @@ func (cs *ConfigSuite) Test_onBeforeSave_uniqueConfigIDIsGeneratedCorrectly(c *C
 func (cs *ConfigSuite) Test_WhenLoaded_addsFunctionToList(c *C) {
 
 	ac := New()
-    var testFunc = func(a *ApplicationConfig) {}
-    ac.WhenLoaded(testFunc)
+	var testFunc = func(a *ApplicationConfig) {}
+	ac.WhenLoaded(testFunc)
 
 	var found bool
-    for _, f := range ac.afterLoad {
-        if reflect.ValueOf(f).Pointer() == reflect.ValueOf(testFunc).Pointer() {
-            found = true
-            break
-        }
-    }
-    c.Assert(found, Equals, true)
+	for _, f := range ac.afterLoad {
+		if reflect.ValueOf(f).Pointer() == reflect.ValueOf(testFunc).Pointer() {
+			found = true
+			break
+		}
+	}
+	c.Assert(found, Equals, true)
 }
-
 
 func (cs *ConfigSuite) Test_OnAfterLoad_executesCallbacksCorrectly(c *C) {
 
-    ac := New()
+	ac := New()
 	var testFunc1 = func(a *ApplicationConfig) {
-        a.LogsEnabled = true
-    }
+		a.LogsEnabled = true
+	}
 	var testFunc2 = func(a *ApplicationConfig) {
-        a.AutoJoin = true
-    }
+		a.AutoJoin = true
+	}
 
-    ac.afterLoad = []func(*ApplicationConfig){testFunc1, testFunc2}
-    ac.OnAfterLoad()
+	ac.afterLoad = []func(*ApplicationConfig){testFunc1, testFunc2}
+	ac.OnAfterLoad()
 
-    c.Assert(ac.LogsEnabled, Equals, true)
-    c.Assert(ac.AutoJoin, Equals, true)
-    c.Assert(len(ac.afterLoad), Equals, 0)
+	c.Assert(ac.LogsEnabled, Equals, true)
+	c.Assert(ac.AutoJoin, Equals, true)
+	c.Assert(len(ac.afterLoad), Equals, 0)
 }
 
 func (cs *ConfigSuite) Test_OnAfterSave_executesCallbacksCorrectly(c *C) {
-    ac := New()
-    flag := false
-    var testFunc = func() {
-        flag = true
-    }
-    ac.afterSave = []func(){testFunc}
+	ac := New()
+	flag := false
+	var testFunc = func() {
+		flag = true
+	}
+	ac.afterSave = []func(){testFunc}
 
-    ac.onAfterSave()
+	ac.onAfterSave()
 
-
-    c.Assert(flag, Equals, true)
-    c.Assert(len(ac.afterSave), Equals, 0)
+	c.Assert(flag, Equals, true)
+	c.Assert(len(ac.afterSave), Equals, 0)
 }
 
 func (cs *ConfigSuite) Test_Save_nonPersistentConfiguration(c *C) {
@@ -179,9 +172,9 @@ func (cs *ConfigSuite) Test_Save_nonPersistentConfiguration(c *C) {
 
 	ac.persistentMode = false
 
-    err := ac.Save(nil)
+	err := ac.Save(nil)
 
-    c.Assert(err, NotNil)
+	c.Assert(err, NotNil)
 }
 
 func (cs *ConfigSuite) Test_DeleteFileIfExists_fileExists(c *C) {
@@ -222,11 +215,9 @@ func (cs *ConfigSuite) Test_CreateBackup_fileExists(c *C) {
 
 	ac.CreateBackup()
 
-
 	backupFile := filepath.Join(filepath.Dir(filename), appConfigFileBackup)
 	_, err = os.Stat(backupFile)
 	c.Assert(err, IsNil)
-
 
 	data, err := ioutil.ReadFile(backupFile)
 	c.Assert(err, IsNil)
@@ -262,181 +253,6 @@ func (cs *ConfigSuite) Test_doAfterSave_addFunctionToList(c *C) {
 	}
 	c.Assert(found, Equals, true)
 }
-
-func (cs *ConfigSuite) Test_GetAutoJoin_returnsSettingValueToAutojoin(c *C) {
-	ac := New()
-
-	expectedAutoJoin := ac.GetAutoJoin()
-
-	c.Assert(ac.AutoJoin, Equals, expectedAutoJoin)
-}
-
-func (cs *ConfigSuite) Test_SetAutoJoin_setsAutojoinToTrue(c *C) {
-	ac := New()
-
-	c.Assert(ac.AutoJoin, Equals, false)
-
-	ac.SetAutoJoin(true)
-
-	c.Assert(ac.AutoJoin, Equals, true)
-}
-
-func (cs *ConfigSuite) Test_SetAutoJoin_setsAutojoinToFalse(c *C) {
-	ac := New()
-	ac.AutoJoin = true
-
-	ac.SetAutoJoin(false)
-
-	c.Assert(ac.AutoJoin, Equals, false)
-}
-
-func (cs *ConfigSuite) Test_GetSuperUser_returnsSettingValueToAutojoinAsSuperuser(c *C) {
-	ac := New()
-
-	expectedAsSuperUser := ac.GetAsSuperUser()
-
-	c.Assert(ac.AsSuperUser, Equals, expectedAsSuperUser)
-}
-
-func (cs *ConfigSuite) Test_SetAutoJoinAsSuperUser_setsAsSuperUserAsTrue(c *C) {
-	ac := New()
-
-	c.Assert(ac.AsSuperUser, Equals, false)
-
-	ac.SetAutoJoinSuperUser(true)
-
-	c.Assert(ac.AsSuperUser, Equals, true)
-}
-
-func (cs *ConfigSuite) Test_SetAutoJoinAsSuperUser_setsAsSuperUserAsFalse(c *C) {
-	ac := New()
-
-	ac.AsSuperUser = true
-
-	ac.SetAutoJoinSuperUser(false)
-
-	c.Assert(ac.AsSuperUser, Equals, false)
-}
-
-func (cs *ConfigSuite) Test_GetPathTor_returnsConfiguredPathToTorBin(c *C) {
-	ac := New()
-
-	ac.PathTor = "path/to/tor/binary"
-
-	expectedPathTor := ac.GetPathTor()
-
-	ac.PathTor = expectedPathTor
-
-	c.Assert(ac.PathTor, Equals, expectedPathTor)
-}
-
-func (cs *ConfigSuite) Test_SetPathTor_setsConfiguredPathToTorBin(c *C) {
-	ac := New()
-
-	expectedPathTor := "path/to/tor/binary"
-
-	ac.SetPathTor(expectedPathTor)
-
-	c.Assert(ac.PathTor, Equals, expectedPathTor)
-}
-
-func (cs *ConfigSuite) Test_GetPathTorSocks_returnsConfiguredPathToTorsocksLib(c *C) {
-	ac := New()
-
-	ac.PathTorsocks = "path/to/torsocks/library"
-
-	expectedPathTorsocks := ac.GetPathTorSocks()
-
-	c.Assert(ac.PathTorsocks, Equals, expectedPathTorsocks)
-}
-
-func (cs *ConfigSuite) Test_SetPathTorSocks_setsConfiguredPathToTorsocksLib(c *C) {
-	ac := New()
-
-	expectedPathTorsocks := "path/to/torsocks/library"
-
-	ac.SetPathTorSocks(expectedPathTorsocks)
-
-	c.Assert(ac.PathTorsocks, Equals, expectedPathTorsocks)
-}
-
-func (cs *ConfigSuite) Test_ShouldEncrypt_returnsIfConfigFileIsEncrypted(c *C) {
-	ac := New()
-
-	expectedEncryptedFile := ac.ShouldEncrypt()
-
-	c.Assert(ac.encryptedFile, Equals, expectedEncryptedFile)
-
-	ac.encryptedFile = true
-	expectedEncryptedFile = ac.ShouldEncrypt()
-
-	c.Assert(ac.encryptedFile, Equals, expectedEncryptedFile)
-}
-
-func (cs *ConfigSuite) Test_SetShouldEncrypt_setsEncryptedFileToTrue(c *C) {
-	ac := New()
-
-	ac.SetShouldEncrypt(true)
-
-	c.Assert(ac.encryptedFile, Equals, true)
-}
-
-func (cs *ConfigSuite) Test_SetShouldEncrypt_setsEncryptedFileToFalse(c *C) {
-	ac := New()
-	ac.encryptedFile = true
-
-	ac.SetShouldEncrypt(false)
-
-	c.Assert(ac.encryptedFile, Equals, false)
-}
-
-func (cs *ConfigSuite) Test_SetShouldEncrypt_noActionWhenAlreadySetToSameValue(c *C) {
-	ac := New()
-	ac.encryptedFile = true
-
-	ac.SetShouldEncrypt(true)
-
-	c.Assert(ac.encryptedFile, Equals, true)
-}
-
-func (cs *ConfigSuite) Test_LogsEnabled_ReturnsCorrectValue(c *C) {
-	ac := New()
-	ac.EnableLogs(true)
-
-	c.Assert(ac.IsLogsEnabled(), Equals, true)
-
-	ac.EnableLogs(false)
-
-	c.Assert(ac.IsLogsEnabled(), Equals, false)
-}
-
-func (cs *ConfigSuite) Test_CustomLogFile_SetAndGet(c *C) {
-	ac := New()
-	expectedLogFile := "/path/to/logs.log"
-
-	ac.SetCustomLogFile(expectedLogFile)
-
-	c.Assert(ac.GetRawLogFile(), Equals, expectedLogFile)
-}
-
-func (cs *ConfigSuite) Test_MumbleBinaryPath_SetAndGet(c *C) {
-	ac := New()
-	expectedPath := "/path/to/mumble"
-
-	ac.SetMumbleBinaryPath(expectedPath)
-
-	c.Assert(ac.MumbleBinaryPath(), Equals, expectedPath)
-}
-
-func (cs *ConfigSuite) Test_PortMumble_SetAndGet(c *C) {
-	ac := New()
-	expectedPort := "12345"
-
-	ac.SetPortMumble(expectedPort)
-
-	c.Assert(ac.GetPortMumble(), Equals, expectedPort)
-}
-
 
 type MockKeySupplier struct {
 	mock.Mock
@@ -487,7 +303,6 @@ func (_m *MockKeySupplier) LastAttemptFailed() {
 func NewMockKeySupplier(t interface {
 	mock.TestingT
 	Cleanup(func())
-
 }) *MockKeySupplier {
 	mock := &MockKeySupplier{}
 	mock.Mock.Test(t)
