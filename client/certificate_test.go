4411242f7577e071a7bac685916701343a14191f
Laura Zambrano <blakami@autonomia.digital>
2024-06-19 08:40:46 -0500

    [L] - Add test cases for extractHostAndPort.

---
+++ client/certificate_test.go
@@ -0,0 +1,42 @@
+package client
+
+import (
+	. "gopkg.in/check.v1"
+)
+
+func (s *clientSuite) Test_extractHostAndPort_returnsAnErrorWhenFailsParsingAddress(c *C) {
+	h, p, err := extractHostAndPort("://")
+	c.Assert(h, Equals, "")
+	c.Assert(p, Equals, "")
+	c.Assert(err, NotNil)
+	c.Assert(err.Error(), Equals, "parse \"://\": missing protocol scheme")
+}
+
+func (s *clientSuite) Test_extractHostAndPort_returnsAnErrorWhenFailsSplittingAddress(c *C) {
+	h, p, err := extractHostAndPort("test")
+	c.Assert(h, Equals, "")
+	c.Assert(p, Equals, "")
+	c.Assert(err, NotNil)
+	c.Assert(err.Error(), Equals, "missing port in address")
+}
+
+func (s *clientSuite) Test_extractHostAndPort_returnsAnErrorWhenPortIsMissing(c *C) {
+	add := "http://test"
+	_, _, err := extractHostAndPort(add)
+	c.Assert(err, NotNil)
+	c.Assert(err.Error(), Equals, "address test: missing port in address")
+}
+
+func (s *clientSuite) Test_extractHostAndPort_returnsAnErrorWhenHostIsMi(c *C) {
+	add := ":420"
+	_, _, err := extractHostAndPort(add)
+	c.Assert(err, NotNil)
+	c.Assert(err.Error(), Equals, "parse \":420\": missing protocol scheme")
+}
+
+func (s *clientSuite) Test_extractHostAndPort_returnsHostAndPortWhenAddressIsCorrect(c *C) {
+	h, p, err := extractHostAndPort("http://test:666")
+	c.Assert(h, Equals, "test")
+	c.Assert(p, Equals, "666")
+	c.Assert(err, IsNil)
+}
