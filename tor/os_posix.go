f702b14175527b6a3a2674079508e01a8bab617b
steffano0 <steffanouromero@gmail.com>
2024-07-22 10:44:01 -0500

    #145 [S] - Change specific OS functions to separate files.

--- tor/os_posix.go
+++ tor/os_posix.go
@@ -2,6 +2,34 @@
 
 package tor
 
+import (
+	"errors"
+
+	log "github.com/sirupsen/logrus"
+)
+
 func searchProxyTool() error {
 	return findTorsocksBinary()
+}
+
+var (
+	// ErrTorsocksNotInstalled is an error to be trown where
+	// torsocks is not installed in the system
+	ErrTorsocksNotInstalled = errors.New("torsocks not available")
+)
+
+func findTorsocksBinary() (fatalErr error) {
+	return findTorsocksInSystem()
+}
+
+func findTorsocksInSystem() (fatalErr error) {
+	path, err := execf.LookPath("torsocks")
+	if err != nil {
+		log.Errorf("Torsocks is not installed in your system: %s", err.Error())
+		return ErrTorsocksNotInstalled
+	}
+
+	log.Debugf("findTorsocksInSystem(%s)", path)
+
+	return nil
 }
