3a9783be05386207a58752a8e010ebcf5443ba88
steffano0 <steffanouromero@gmail.com>
2024-07-23 09:44:19 -0500

    #146 [S] - Add function to search for proxychains binary on windows.

--- tor/os_windows.go
+++ tor/os_windows.go
@@ -1,5 +1,24 @@
 package tor
 
+import (
+	log "github.com/sirupsen/logrus"
+)
+
 func searchProxyTool() error {
+	return findProxychainsBinary()
+}
+
+func findProxychainsBinary() (fatalErr error) {
+	return findProxychainsInSystem()
+}
+
+func findProxychainsInSystem() (fatalErr error) {
+	path, err := execf.LookPath("proxychains_win32_x64")
+	if err != nil {
+		log.Errorf("Proxychains is not installed in your system: %s", err.Error())
+		return ErrProxychainsNotInstalled
+	}
+	log.Debugf("findProxychainsInSystem(%s)", path)
+
 	return nil
 }
