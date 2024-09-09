cb414c719a786ffb9755848b3c920bf8900640be
steffano0 <steffanouromero@gmail.com>
2024-09-09 09:44:07 -0500

    #169 [SL] - Delete comment section about torsocks.

--- client/client_posix.go
+++ client/client_posix.go
@@ -5,9 +5,6 @@ package client
 func (c *client) binaryEnv() []string {
 	// This is a temporary fix for making sure that
 	// Mumble doesn't run under Wayland.
-	// Once the torsocks problem with Wayland has been
-	// fixed, we can make this conditional on the version
-	// of torsocks
 	env := []string{"QT_QPA_PLATFORM=xcb"}
 	if c.isValid && c.binary != nil {
 		return append(env, c.binary.envIfBundle()...)
