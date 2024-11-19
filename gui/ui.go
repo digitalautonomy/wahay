00267a3e582e37abddb17c1807f9a3d5414577ba
steffano0 <steffanouromero@gmail.com>
2024-11-19 17:08:36 -0500

    #172 [S] - Disable the settings button when there are errors during wahay startup.

--- gui/ui.go
+++ gui/ui.go
@@ -230,7 +230,9 @@ func (u *gtkUI) disableMainWindowControls(builder *uiBuilder) {
 
 	btnHostMeeting := builder.get("btnHostMeeting").(gtki.Button)
 	btnJoinMeeting := builder.get("btnJoinMeeting").(gtki.Button)
+	btnSettings := builder.get("btnSettings").(gtki.Button)
 
+	btnSettings.SetSensitive(false)
 	btnHostMeeting.SetSensitive(false)
 	btnJoinMeeting.SetSensitive(false)
 }
