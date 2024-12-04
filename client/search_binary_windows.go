37472d28a129224c42eb6ae4ea5e59847d57d46e
steffano0 <steffanouromero@gmail.com>
2024-12-04 11:46:11 -0500

    #176 - Search for mumble binary on PATH and program files x86

--- client/search_binary_windows.go
+++ client/search_binary_windows.go
@@ -14,10 +14,22 @@ const (
 var execLookPath = exec.LookPath
 
 func searchBinaryInSystem() (*binary, error) {
-
+	//Here we ignore the error because we handle the empty string returned.
+	path, _ := execLookPath("mumble.exe")
 	programFilesDir := os.Getenv("PROGRAMFILES")
+	programFilesX86Dir := os.Getenv("PROGRAMFILES(X86)")
+	dirs := []string{
+		programFilesX86Dir,
+		programFilesDir,
+		path,
+	}
 
-	b := isThereAnAvailableBinary(programFilesDir)
+	for _, d := range dirs {
+		b := isThereAnAvailableBinary(d)
+		if b != nil && b.isValid {
+			return b, nil
+		}
+	}
 
-	return b, nil
+	return nil, nil
 }
