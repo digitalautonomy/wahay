c2c8dcfff2cf3c6235ee16e072f460e9c2b8592b
Laura <malaura.zam.pa@gmail.com>
2025-03-14 16:55:34 -0500

    #191 [SL] - Detect current system theme for Windows.

--- gui/system_theme_detector_windows.go
+++ gui/system_theme_detector_windows.go
@@ -1,7 +1,25 @@
 package gui
 
+import (
+	log "github.com/sirupsen/logrus"
+	"golang.org/x/sys/windows/registry"
+)
+
 func isDarkMode() bool {
-	return true
+	key, err := registry.OpenKey(registry.CURRENT_USER, `SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize`, registry.QUERY_VALUE)
+	if err != nil {
+		log.Printf("Failed to open registry key: %v", err)
+		return false
+	}
+	defer key.Close()
+
+	value, _, err := key.GetIntegerValue("AppsUseLightTheme")
+	if err != nil {
+		log.Printf("Failed to read registry value: %v", err)
+		return false
+	}
+
+	return value == 0
 }
 
 func (s *settings) monitorSystemStyleChanges() {}
